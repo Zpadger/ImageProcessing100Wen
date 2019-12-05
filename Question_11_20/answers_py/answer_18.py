ae74e95f4eede06c932b1ac805692872524654cd
Zpadger <31385823+Zpadger@users.noreply.github.com>
2019-12-05 10:05:04 +0800

    修正了程序的bug
        H, W, C = img.shape
    ValueError: not enough values to unpack (expected 3, got 2)

--- Question_11_20/answers_py/answer_18.py
+++ Question_11_20/answers_py/answer_18.py
@@ -15,7 +15,11 @@ def BGR2GRAY(img):
 
 # emboss filter
 def emboss_filter(img, K_size=3):
-	H, W, C = img.shape
+	if len(img.shape) == 3:
+		H, W, C = img.shape
+	else:
+		img = np.expand_dims(img, axis=-1)
+		H, W, C = img.shape
 
 	# zero padding
 	pad = K_size // 2
